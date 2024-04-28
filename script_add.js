5c6b9bedb5b6a892173765cc6c4c08f0b652b7cc
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-04-28 23:26:04 +0100

    second commit

--- script_add.js
+++ script_add.js
@@ -1,13 +1,21 @@
 let n1 = document.getElementById('n1')
 let n2 = document.getElementById('n2')
+let n3 = document.getElementById('n3')
 let sum = document.getElementById('sum')
 let total = document.getElementById('total')
 
 function addition() {
     let a = Number(n1.value)
     let b = Number(n2.value)
+    let c = Number(n3.value)
 
-    let res = a + b
+    let res
+
+    if (c === 0){
+        res = a + b 
+    }else{
+        res = a + b + c
+    }
 
     sum.value = res
 }
--- script_multiple.js
+++ script_multiple.js
@@ -1,13 +1,21 @@
 let n1 = document.getElementById('n1')
 let n2 = document.getElementById('n2')
+let n3 = document.getElementById('n3')
 let multiple = document.getElementById('multiple')
 let total = document.getElementById('total')
 
 function multiplication() {
     let a = Number(n1.value)
     let b = Number(n2.value)
+    let c = Number(n3.value)
 
-    let res = a * b
+    let res
+
+    if (c === 0){
+        res = a * b
+    }else{
+        res = a * b * c
+    }
 
     multiple.value = res
 }
